90c56841acbdeb4036b9c75e62f384d11e9d2fd6
sujeong816 <sujeong1517@naver.com>
2022-12-31 22:22:06 +0900

    event item & list

---
+++ components/events/event-list.js
@@ -0,0 +1,20 @@
+import EventItem from "./event-item";
+import classes from './event-list.module.css';
+
+function EventList(props){
+    const {items} = props;
+    return(
+        <ul className={classes.list}>
+            {items.map(event => 
+                <EventItem key={event.id}
+                           id={event.id} 
+                           title={event.title} 
+                           image={event.image}
+                           location={event.location}
+                           date={event.date}
+                />)}
+        </ul>
+    );
+}
+
+export default EventList;
